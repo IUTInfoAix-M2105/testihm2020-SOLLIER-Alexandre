aa64c31d96d6b59a98fcd60378139698a9570fa6
Kuruyia <alexandresollier@gmail.com>
2020-06-12 09:32:22 +0200

    TraceurDeFonction: Setupped the window, and the expression textfield

--- src/main/java/fr/univ_amu/iut/ihm/TraceurDeFonction.java
+++ src/main/java/fr/univ_amu/iut/ihm/TraceurDeFonction.java
@@ -2,8 +2,42 @@ package fr.univ_amu.iut.ihm;
 
 import fr.univ_amu.iut.utilitaires.Analyseur;
 import fr.univ_amu.iut.utilitaires.ErreurDeSyntaxe;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class TraceurDeFonction {
+public class TraceurDeFonction extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    stage.setTitle("Traceur de fonction");
+    stage.setWidth(600);
+    stage.setHeight(650);
+    stage.setResizable(false);
+
+    GridPane root = new GridPane();
+    root.setHgap(10);
+
+    Label expressionLabel = new Label("Expression:");
+    TextField expressionTextField = new TextField("exp(-x * 0.2) * sin(x)");
+    root.add(expressionLabel, 0, 0);
+    root.add(expressionTextField, 1, 0);
+
+    Scene scene = new Scene(root);
+
+    stage.setScene(scene);
+    stage.show();
+  }
 
   void calculCoeffTransformationsAffines() {
   }
